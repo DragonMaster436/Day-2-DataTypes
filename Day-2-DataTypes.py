8e0cb40522f80a28c8b0fc23f7ff1c4cc00de03b
DragonMaster436 <96381483+DragonMaster436@users.noreply.github.com>
2024-02-17 18:39:37 -0600

    Add files via upload

---
+++ Day-2-DataTypes.py
@@ -0,0 +1,10 @@
+# 🚨 Don't change the code below 👇
+two_digit_number = input("Type a two digit number: ")
+# 🚨 Don't change the code above 👆
+
+####################################
+#Write your code below this line 👇
+num_1 = int(two_digit_number[0])
+num_2 = int(two_digit_number[1])
+total = num_1 + num_2
+print(total)
